38e5239e1cf87638527bf2622606e6bd6e6867c1
repl.it user <replituser@example.com>
2020-11-13 16:00:00 +0000

    working Code

--- Life.java
+++ Life.java
@@ -1,4 +1,8 @@
 public class Life implements ILife {
+
+  private final int ROWS = 5;
+  private final int COLUMNS = 5;
+  private char[][] lifePhase = new char[ROWS][COLUMNS];
   
   public static void main(String[] args) {
     Life l = new Life(new String[] {  "     ",
@@ -18,38 +22,72 @@ public class Life implements ILife {
     this();
     for (int y = 0; y < setup.length; y++)
       for (int x = 0; x < setup[y].length(); x++)
-        if (setup[y].charAt(x) != ' ')
-          setAlive(x, y);
+        if (setup[y].charAt(x) != ' ') setAlive(x, y);
+        else setDead(x,y);
   }
 
 
   @Override
   public void nukeAll() {
     // TODO Auto-generated method stub
-
+    for (int y = 0; y < ROWS; y++)
+      for (int x = 0; x < COLUMNS; x++)
+      setDead(x,y);
   }
 
   @Override
   public void setAlive(int x, int y) {
     // TODO Auto-generated method stub
-
+    lifePhase[x][y] = '*';
   }
 
   @Override
   public void setDead(int x, int y) {
     // TODO Auto-generated method stub
-
+    lifePhase[x][y] = ' ';
   }
 
   @Override
   public boolean isAlive(int x, int y) {
-    // TODO Auto-generated method stub
+    if(lifePhase[x][y] == '*')
+      return true;
     return false;
   }
 
   @Override
   public ILife nextGeneration() {
     // TODO Auto-generated method stub
-    return null;
+    return calculateNextGeneration();
+  }
+
+  private Life calculateNextGeneration() {
+      Life nextGen = new Life();
+      for (int y = 0; y < ROWS; y++){
+      for (int x = 0; x < COLUMNS; x++){
+      if (lifePhase[y][x] == '*' && (numberOfLivingCellsAround(y, x) == 2 && numberOfLivingCellsAround(y, x) == 3)) { // Living cell
+        nextGen.setAlive(x, y);
+      } else if (lifePhase[y][x] == ' ' && numberOfLivingCellsAround(y, x) == 3) {
+        nextGen.setAlive(x, y);
+      } else {
+        nextGen.setDead(x, y);
+      }
+      }
+      }
+      return nextGen;
+      }
+    
+  private int numberOfLivingCellsAround(int x, int y) {
+    int aliveCells = 0;
+    for(int row = y-1; row <= y+1; row++){
+      if(row>=0 && row < ROWS){
+      for(int column = x-1; column <= x+1; column++){
+        if(column>=0 && column < COLUMNS)
+        if(lifePhase[row][column]=='*') aliveCells++;
+      }
+      }
+    }
+    if(lifePhase[y][x]=='*') aliveCells--;
+    System.out.println(x + ", " + y + ", " + aliveCells);
+    return aliveCells;
   }
 }
--- LifeTest.java
+++ LifeTest.java
@@ -17,21 +17,50 @@ public class LifeTest {
 
         // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
         assertTrue(nextGen.isAlive(1, 1));
+        System.out.println("Test1 done");
     }
 
 
     @Test
     public void destroyLonelyCell() {
+       Life l = new Life();
+        l.setAlive(0, 0);
+        l.setAlive(0, 1);
+        l.setAlive(0, 2);
+        l.setAlive(0, 4);
+         ILife nextGen = l.nextGeneration();
+      assertFalse(nextGen.isAlive(0, 4));
+      System.out.println("Test2 done");
     }
 
 
-    @Test
+   @Test
     public void keepAliveCell() {
+      Life l = new Life();
+        l.setAlive(0, 0);
+        l.setAlive(0, 1);
+        l.setAlive(0, 2);
+        l.setAlive(1, 1);
+        ILife nextGen = l.nextGeneration();
+      assertTrue(nextGen.isAlive(0, 1));
+      System.out.println("Test3 done");
     }
 
 
     @Test
     public void destroyCrowdedCell() {
+      Life l = new Life();
+        l.setAlive(0, 0);
+        l.setAlive(0, 1);
+        l.setAlive(0, 2);
+        l.setAlive(1, 1);
+        l.setAlive(1, 0);
+        l.setAlive(1, 2);
+        l.setAlive(2, 2);
+        l.setAlive(2, 0);
+      ILife nextGen = l.nextGeneration();
+      assertFalse(nextGen.isAlive(1, 1));
+      System.out.println("Test4 done");
     }
 
 
